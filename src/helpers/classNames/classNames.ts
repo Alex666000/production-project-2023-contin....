3ca8d2b9bcec747db6bf68d29c2dc1c2a31e5b56
Aleksandr_UB <bogdanov777000@mail.ru>
2023-05-30 20:43:55 +0300

    classNames (ANALOG===HELPER)

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,26 @@
+
+type Mods = Record<string, boolean | string>
+
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+    // возвращаем строку классов
+    return [
+        cls,
+        ...additional,
+        ...Object.entries(mods)
+            .filter(([className, value]) => Boolean(value))
+            .map(([className]) => className)
+    ]
+        .join(' ');
+}
+// classNames('remove-btn', {hovered: true,selecteble: true, red: true}, ['pdg'])
+
+
+/*
+mods - объект,  ключ название класса а значение булеан флаг  если true класс добавим иначе нет
+additional - массив дополнительных классов
+
+Record из ТС - в качестве ключа строка а значение булеан
+ */
+
+
+
